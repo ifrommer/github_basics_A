64accc97570c2c16d969cc89f6ec9454b8c9e113
Christian Gottlieb <christian.k.gottlieb@uscga.edu>
2026-01-14 19:38:57 -0500

    Update recipes.py

--- recipes.py
+++ recipes.py
@@ -1,5 +1,6 @@
 recipes = {
     "apple pie": ["apples", "flour", "sugar"],
     "pancakes": ["flour", "milk", "eggs"],
-    "cheesecake": ["graham cracker crust", "cream cheese", "sugar"]
+    "cheesecake": ["graham cracker crust", "cream cheese", "sugar"],
+    "bannana nut bread": ["bananas", "sugar", "flour", "baking soda"]
 }
