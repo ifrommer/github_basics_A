42f1fe63b79cc76c0a7d34482a44d1beefbeb640
Christian Gottlieb <christian.k.gottlieb@uscga.edu>
2026-01-15 20:53:40 -0500

    Update recipes.py

--- recipes.py
+++ recipes.py
@@ -4,5 +4,7 @@ recipes = {
     "cheesecake": ["graham cracker crust", "cream cheese", "sugar"],
     "bannana nut bread": ["bananas", "sugar", "flour", "baking soda"],
     "blueberry muffins": ["blueberries", "flour", "sugar", "baking soda"]
-    "chocolate chip cookie": ["chocolate chips", "flour", "sugar"], "coffee cake": ["sugar, cinnamon, flour, baking soda]
+    "chocolate chip cookie": ["chocolate chips", "flour", "sugar"], 
+    "coffee cake": ["sugar", "cinnamon", "flour", "baking soda"],
+    "lemonade": ["lemons","water","sugar"]
 }
