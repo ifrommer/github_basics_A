e03058d42626824394038b789c4d2e132906aba9
beholt40 <brennan.e.holt@uscga.edu>
2026-01-20 11:40:49 -0500

    Update Colbrin_and_Brennan.py.py

--- Colbrin_and_Brennan.py.py
+++ Colbrin_and_Brennan.py.py
@@ -7,3 +7,6 @@ This is a temporary script file.
 #This is a file for Brennan and Colbrin to do Homework 1 in.
 print("Hello world, this is now DST")
 
+# This is another push test
+print("IT WORKS!!!")
+
