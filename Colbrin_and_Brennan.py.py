75577234575f67f6ef8f19b4cbdaf87b9fc30605
Colburner88 <coleblanchette88@gmail.com>
2026-01-20 11:38:12 -0500

    Brennan and Colbrin created

---
+++ Colbrin_and_Brennan.py.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#This is a file for Brennan and Colbrin to do Homework 1 in.
+print("Hello world, this is now DST")
+
